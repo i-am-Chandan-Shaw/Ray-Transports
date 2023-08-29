abe5698a0c804da32039f7b0e9792f433ab1cb6f
i-am-Chandan-Shaw <chandan.shaw2050@gmail.com>
2023-08-30 00:25:16 +0530

    added User Filter in transaction dashboard

--- src/app/home/transactions/transactions.component.ts
+++ src/app/home/transactions/transactions.component.ts
@@ -13,43 +13,42 @@ export class TransactionsComponent implements OnInit {
 
   constructor(private services:SharedService){}
 
-  public userList=[
-    {
-      id:0,
-      name:'John',
-      email:'john@gmail.com'
-    },
-    {
-      id:2,
-      name:'Mark',
-      email:'mark@gmail.com'
-    },
-    {
-      id:3,
-      name:'Sam',
-      email:'samb@gmail.com'
-    },
-    {
-      id:4,
-      name:'Lilly',
-      email:'lilly@gmail.com'
-    },
-  ]
+  public userList:any[]=[]
   public filterList:filter[] = filterList
   public sortList:filter[]=sortOption
   public myMath = Math;
 
   transactionDetails:any=[]
+  transactionDetailsList:any[]=[]
 
   ngOnInit(): void {
     this.services.getAllTransactionDetails().subscribe({
       next: (res) => {
         this.transactionDetails = res
+        this.transactionDetailsList = JSON.parse(JSON.stringify(this.transactionDetails))
+        for(let item of this.transactionDetails){
+          this.userList.push({
+            id:item.customerId,
+            name:item.customerName,
+          })
+        }
+        
+        //this is how we filter out array of object
+        this.userList=this.userList.filter((v,i,a)=>a.findIndex(v2=>(v2.id===v.id))===i)
       },error: (err) => {
         console.log(err);
         
       }
     })
+    }
+
+    onValueSelected(e:any){
+      this.transactionDetails = []
+      for(let item of this.transactionDetailsList){
+        if(e.name == item.customerName){
+          this.transactionDetails.push(item)
+        }
+      }
     }
   }
 
